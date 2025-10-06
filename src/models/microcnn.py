eb682316db8f833528ab5c4ca0386048171f0c7d
Filippenko Pavel <filippenko.ps@phystech.edu>
2025-10-06 17:30:50 +0300

    fix almost all of the backbone arch

--- src/models/microcnn.py
+++ src/models/microcnn.py
@@ -1,33 +1,46 @@
 import torch.nn as nn
 
+
 class MicroCNN(nn.Module):
     def __init__(self, num_classes=10, in_features=64):
         super(MicroCNN, self).__init__()
-        
+
         self.num_classes = num_classes
-        
+
         self.features = nn.Sequential(
-            nn.Conv1d(in_features, 4, kernel_size=3, padding=1),
+            # Первый блок
+            nn.Conv1d(in_features, 12, kernel_size=3, padding=1),
+            nn.BatchNorm1d(12),
             nn.ReLU(inplace=True),
             nn.MaxPool1d(kernel_size=2, stride=2),
-            nn.Conv1d(4, 8, kernel_size=3, padding=1),
+
+            # Второй блок
+            nn.Conv1d(12, 24, kernel_size=3, padding=1),
+            nn.BatchNorm1d(24),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+
+            # Третий блок
+            nn.Conv1d(24, 36, kernel_size=3, padding=1),
+            nn.BatchNorm1d(36),
             nn.ReLU(inplace=True),
             nn.MaxPool1d(kernel_size=2, stride=2),
         )
-        # Заглушка для классификатора
+
         self.avgpool = nn.AdaptiveAvgPool1d(1)
         self.flatten = nn.Flatten()
-                
+
         self.classifier = nn.Sequential(
-                nn.Linear(8, 16),
-                nn.ReLU(inplace=True),
-                nn.Linear(16, self.num_classes)
-            ) 
-        
+            nn.Dropout(0.3),
+            nn.Linear(36, 24),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+            nn.Linear(24, self.num_classes)
+        )
+
     def forward(self, x):
         x = self.features(x)
         x = self.avgpool(x)
         x = self.flatten(x)
-            
         x = self.classifier(x)
         return x
