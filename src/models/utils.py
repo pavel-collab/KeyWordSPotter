92c37f2e0b7c088c1290b304c6d0254bec3fd4ba
Filippenko Pavel <filippenko.ps@phystech.edu>
2025-10-06 20:43:30 +0300

    add ResNet

--- src/models/utils.py
+++ src/models/utils.py
@@ -5,6 +5,7 @@ from src.models.nanocnn import NanoCNN
 from src.models.tinycnn import TinyCNN
 from src.models.customcnn import CustomNet
 from src.models.depwisecnn import MobileNetV1_1D, SimpleMobileNet1D
+from src.models.resnet import  SmallResNet
 
 model_map = {
     # "ResNet18Backbone": ResNet18Backbone,
@@ -16,5 +17,6 @@ model_map = {
     "Conv1dNet": Conv1dNet,
     "CustomNet": CustomNet,
     "MobileNetV1_1D": MobileNetV1_1D,
-    "SimpleMobileNet1D": SimpleMobileNet1D
+    "SimpleMobileNet1D": SimpleMobileNet1D,
+    "SmallResNet": SmallResNet
 }
